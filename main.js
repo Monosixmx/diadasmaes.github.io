1018a466877e58cfd201d351dce06254fb05f821
Monosixmx <165345491+Monosixmx@users.noreply.github.com>
2024-05-11 14:29:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,495 @@
+const body = document.main;
+const voltar = document.getElementById('volta');
+
+const btnana = document.getElementById('btn1');
+const textana = document.getElementById('textana');
+
+
+const btnanastacio = document.getElementById('btn2');
+const textanastacio = document.getElementById('textanastacio');
+
+const btnbruno = document.getElementById('btn3');
+const textbruno = document.getElementById('textbruno');
+
+const btncaique = document.getElementById('btn4');
+const textcaique = document.getElementById('textcaique');
+
+const btnguilherme = document.getElementById('btn5');
+const textguilherme = document.getElementById('textguilherme');
+
+
+const btndaniel = document.getElementById('btn6');
+const textdaniel = document.getElementById('textdaniel');
+
+const btndavid = document.getElementById('btn7');
+const textdavid = document.getElementById('textdavid');
+
+const btndebora = document.getElementById('btn8');
+const textdebora = document.getElementById('textdebora');
+
+const btneduardo = document.getElementById('btn9');
+const texteduardo = document.getElementById('texteduardo');
+
+
+const btnedu = document.getElementById('btn10');
+const textedu = document.getElementById('textedu');
+
+const btnelisson = document.getElementById('btn11');
+const textelisson = document.getElementById('textelisson');
+
+const btnerick = document.getElementById('btn12');
+const texterick = document.getElementById('texterick');
+
+const btnflavio = document.getElementById('btn13');
+const textflavio = document.getElementById('textflavio');
+
+
+const btnyasmim = document.getElementById('btn14');
+const textyasmim = document.getElementById('textyasmim');
+
+const btngabriele = document.getElementById('btn15');
+const textgabriele = document.getElementById('textgabriele');
+
+const btngabriel = document.getElementById('btn16');
+const textgabriel = document.getElementById('textgabriel');
+
+const btngustavo = document.getElementById('btn17');
+const textgustavo = document.getElementById('textgustavo');
+
+
+const btnhiuston = document.getElementById('btn18');
+const texthiuston = document.getElementById('texthiuston');
+
+const btnismael = document.getElementById('btn19');
+const textismael = document.getElementById('textismael');
+
+const btnjardson = document.getElementById('btn20');
+const textjardson = document.getElementById('textjardson');
+
+const btnjean = document.getElementById('btn21');
+const textjean = document.getElementById('textjean');
+
+
+const btnjoao = document.getElementById('btn22');
+const textjoao = document.getElementById('textjoao');
+
+const btnmoacir = document.getElementById('btn23');
+const textmoacir = document.getElementById('textmoacir');
+
+const btnkadson = document.getElementById('btn24');
+const textkadson = document.getElementById('textkadson');
+
+const btnjuan = document.getElementById('btn25');
+const textjuan = document.getElementById('textjuan');
+
+
+const btnkaio = document.getElementById('btn26');
+const textkaio = document.getElementById('textkaio');
+
+const btnkayke= document.getElementById('btn27');
+const textkayke = document.getElementById('textkayke');
+
+const btnlaryssa = document.getElementById('btn28');
+const textlaryssa = document.getElementById('textlaryssa');
+
+const btnlays = document.getElementById('btn29');
+const textlays = document.getElementById('textlays');
+
+
+const btnleonardo = document.getElementById('btn30');
+const textleonardo = document.getElementById('textleonardo');
+
+const btnluan = document.getElementById('btn31');
+const textluan = document.getElementById('textluan');
+
+const btnlucas = document.getElementById('btn32');
+const textlucas = document.getElementById('textlucas');
+
+const btnlucia = document.getElementById('btn33');
+const textlucia = document.getElementById('textlucia');
+
+
+const btnluis = document.getElementById('btn34');
+const textluis = document.getElementById('textluis');
+
+const btnluiz = document.getElementById('btn35');
+const textluiz = document.getElementById('textluiz');
+
+const btnclara = document.getElementById('btn36');
+const textclara = document.getElementById('textclara');
+
+const btndayane = document.getElementById('btn37');
+const textdayane = document.getElementById('textdayane');
+
+
+const btnluisa = document.getElementById('btn38');
+const textluisa = document.getElementById('textluisa');
+
+const btnmarieva = document.getElementById('btn39');
+const textmarieva = document.getElementById('textmarieva');
+
+const btnmatheus= document.getElementById('btn40');
+const textmatheus = document.getElementById('textmatheus');
+
+const btnpablo = document.getElementById('btn41');
+const textpablo = document.getElementById('textpablo');
+
+const btnpaulo = document.getElementById('btn42');
+const textpaulo = document.getElementById('textpaulo');
+
+
+const btnqueren= document.getElementById('btn43');
+const textqueren = document.getElementById('textqueren');
+
+
+const btnrenan = document.getElementById('btn44');
+const textrenan = document.getElementById('textrenan');
+
+const btnhelen = document.getElementById('btn45');
+const texthelen = document.getElementById('texthelen');
+
+const btnbrian = document.getElementById('btn46');
+const textbrian = document.getElementById('textbrian');
+
+
+voltar.addEventListener('click', function(){
+    voltar.style.display = 'none';
+    textana.style.display = 'none';
+    textanastacio.style.display = 'none';
+    textbruno.style.display = 'none';
+    textcaique.style.display = 'none';
+    textguilherme.style.display = 'none';
+    textdaniel.style.display = 'none';
+    textdavid.style.display = 'none';
+    textdebora.style.display = 'none';
+    texteduardo.style.display = 'none';
+    textedu.style.display = 'none';
+    textelisson.style.display = 'none';
+    texterick.style.display = 'none';
+    textflavio.style.display = 'none';
+    textyasmim.style.display = 'none';
+    textgabriele.style.display = 'none';
+    textgabriel.style.display = 'none';
+    textgustavo.style.display = 'none';
+    texthiuston.style.display = 'none';
+    textismael.style.display = 'none';
+    textjardson.style.display = 'none';
+    textjean.style.display = 'none';
+    textjoao.style.display = 'none';
+    textmoacir.style.display = 'none';
+    textkadson.style.display = 'none';
+    textjuan.style.display = 'none';
+    textkaio.style.display = 'none';
+    textkayke.style.display = 'none';
+    textlaryssa.style.display = 'none';
+    textlays.style.display = 'none';
+    textleonardo.style.display = 'none';
+    textluan.style.display = 'none';
+    textlucas.style.display = 'none';
+    textlucia.style.display = 'none';
+    textluis.style.display = 'none';
+    textluiz.style.display = 'none';
+    textclara.style.display = 'none';
+    textdayane.style.display = 'none';
+    textluisa.style.display = 'none';
+    textmarieva.style.display = 'none';
+    textmatheus.style.display = 'none';
+    textpablo.style.display = 'none';
+    textpaulo.style.display = 'none';
+    textqueren.style.display = 'none';
+    textrenan.style.display = 'none';
+    texthelen.style.display = 'none';
+    textbrian.style.display = 'none';
+    
+});
+
+btnana.addEventListener('click', function(){
+    textana.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnanastacio.addEventListener('click', function(){
+    textanastacio.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnbruno.addEventListener('click', function(){
+    textbruno.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btncaique.addEventListener('click', function(){
+    textcaique.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnguilherme.addEventListener('click', function(){
+    textguilherme.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btndaniel.addEventListener('click', function(){
+    textdaniel.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btndavid.addEventListener('click', function(){
+    textdavid.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btndebora.addEventListener('click', function(){
+    textdebora.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btneduardo.addEventListener('click', function(){
+    texteduardo.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnedu.addEventListener('click', function(){
+    textedu.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnelisson.addEventListener('click', function(){
+    textelisson.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnerick.addEventListener('click', function(){
+    texterick.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnflavio.addEventListener('click', function(){
+    textflavio.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnyasmim.addEventListener('click', function(){
+    textyasmim.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btngabriele.addEventListener('click', function(){
+    textgabriele.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btngabriel.addEventListener('click', function(){
+    textgabriel.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btngustavo.addEventListener('click', function(){
+    textgustavo.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnhiuston.addEventListener('click', function(){
+    texthiuston.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnismael.addEventListener('click', function(){
+    textismael.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnjardson.addEventListener('click', function(){
+    textjardson.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnjean.addEventListener('click', function(){
+    textjean.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnjoao.addEventListener('click', function(){
+    textjoao.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnmoacir.addEventListener('click', function(){
+    textmoacir.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnjuan.addEventListener('click', function(){
+    textjuan.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnkadson.addEventListener('click', function(){
+    textkadson.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnkaio.addEventListener('click', function(){
+    textkaio.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnkayke.addEventListener('click', function(){
+    textkayke.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnlaryssa.addEventListener('click', function(){
+    textlaryssa.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnlays.addEventListener('click', function(){
+    textlays.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnleonardo.addEventListener('click', function(){
+    textleonardo.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnluan.addEventListener('click', function(){
+    textluan.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnlucas.addEventListener('click', function(){
+    textlucas.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnlucia.addEventListener('click', function(){
+    textlucia.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnluis.addEventListener('click', function(){
+    textluis.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnluiz.addEventListener('click', function(){
+    textluiz.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnclara.addEventListener('click', function(){
+    textclara.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btndayane.addEventListener('click', function(){
+    textdayane.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnluisa.addEventListener('click', function(){
+    textluisa.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnmarieva.addEventListener('click', function(){
+    textmarieva.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnmatheus.addEventListener('click', function(){
+    textmatheus.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnpablo.addEventListener('click', function(){
+    textpablo.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnpaulo.addEventListener('click', function(){
+    textpaulo.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnqueren.addEventListener('click', function(){
+    textqueren.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnrenan.addEventListener('click', function(){
+    textrenan.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+btnhelen.addEventListener('click', function(){
+    texthelen.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+}); 
+
+btnbrian.addEventListener('click', function(){
+    textbrian.style.display = 'flex';
+    voltar.style.display = 'block';
+    body.style.background = 'rgba(0, 0, 0, 0.459)';
+});
+
+document.getElementById('inputjs').addEventListener('input', function() {
+    const searchTerm = this.value.toLowerCase(); // Obtém o valor digitado e converte para minúsculas
+    const alunos = document.querySelectorAll('ul li'); // Seleciona todos os itens da lista
+    alunos.forEach(function(aluno) {
+        const alunoText = aluno.textContent.toLowerCase(); // Obtém o texto do item e converte para minúsculas
+        if (alunoText.includes(searchTerm)) {
+            aluno.style.display = 'block';
+            aluno.style.margin = '0, auto';// Mostra o item se ele corresponder ao termo de busca
+        } else {
+            aluno.style.display = 'none'; // Oculta o item se ele não corresponder
+        }
+    });
+});
+
